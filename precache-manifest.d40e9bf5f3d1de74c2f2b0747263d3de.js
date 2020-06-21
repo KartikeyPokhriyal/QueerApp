5fd9a457a7f2d8783c3c757c90004a08032a97c1
Kartikey Pokhriyal <kartikey.pokhriyal@thoughtworks.com>
2020-06-22 00:47:51 +0530

    Updates

---
+++ precache-manifest.d40e9bf5f3d1de74c2f2b0747263d3de.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b69aafe517bd7d637d981e81f657df",
+    "url": "/QueerApp/index.html"
+  },
+  {
+    "revision": "1c4d4818e814afa788e1",
+    "url": "/QueerApp/static/css/main.68b288f4.chunk.css"
+  },
+  {
+    "revision": "434ce99733d8a250e4c0",
+    "url": "/QueerApp/static/js/2.660a3bb4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/QueerApp/static/js/2.660a3bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c4d4818e814afa788e1",
+    "url": "/QueerApp/static/js/main.c9ca90d4.chunk.js"
+  },
+  {
+    "revision": "9b5fcbae325c142a926a",
+    "url": "/QueerApp/static/js/runtime-main.b11fbb57.js"
+  },
+  {
+    "revision": "4e97bc45157fbba6619f821786fd9b19",
+    "url": "/QueerApp/static/media/377.4e97bc45.jpg"
+  },
+  {
+    "revision": "52cf93e2034943c7409407be1a04c2ac",
+    "url": "/QueerApp/static/media/accept.52cf93e2.png"
+  },
+  {
+    "revision": "cd73610f1b7a5d35823c4dda51072db3",
+    "url": "/QueerApp/static/media/america.cd73610f.gif"
+  },
+  {
+    "revision": "d29a760cfa9e586841a8797d76f77d40",
+    "url": "/QueerApp/static/media/babur.d29a760c.jpg"
+  },
+  {
+    "revision": "f605519e480730c4b99e41dad9d6c84b",
+    "url": "/QueerApp/static/media/bombay.f605519e.jpg"
+  },
+  {
+    "revision": "a6e1fe0936747c39cc5330b5f49a1b78",
+    "url": "/QueerApp/static/media/california.a6e1fe09.jpg"
+  },
+  {
+    "revision": "fb216ffbec085742e37b21fe3e64b105",
+    "url": "/QueerApp/static/media/correct.fb216ffb.gif"
+  },
+  {
+    "revision": "b765d54eb38044619afea0882c7fb6ac",
+    "url": "/QueerApp/static/media/ellen.b765d54e.jpeg"
+  },
+  {
+    "revision": "37afd4340f0eee3639af6ae197db6dd0",
+    "url": "/QueerApp/static/media/gaymen.37afd434.jpg"
+  },
+  {
+    "revision": "c2b3901aff042551accc2482dd985d39",
+    "url": "/QueerApp/static/media/madonna.c2b3901a.jpg"
+  },
+  {
+    "revision": "4b7128dd8a2410de0eec35afee295939",
+    "url": "/QueerApp/static/media/mariah.4b7128dd.gif"
+  },
+  {
+    "revision": "e77c50899a130da1ff8f0527e5f853df",
+    "url": "/QueerApp/static/media/pronouns.e77c5089.jpg"
+  },
+  {
+    "revision": "b544d4ffb6d662a8d6b680368361561c",
+    "url": "/QueerApp/static/media/reject.b544d4ff.png"
+  },
+  {
+    "revision": "d5e4727353b7a6bd848ad305c153a07c",
+    "url": "/QueerApp/static/media/sasha.d5e47273.gif"
+  },
+  {
+    "revision": "a24df539cfdabc1f7ece393a4032465e",
+    "url": "/QueerApp/static/media/stoner.a24df539.svg"
+  },
+  {
+    "revision": "3e8262015d7890c9b1e73430b94f81d4",
+    "url": "/QueerApp/static/media/therapist.3e826201.jpg"
+  },
+  {
+    "revision": "5c7c26f059287b36c7b025f7c6b53863",
+    "url": "/QueerApp/static/media/try.5c7c26f0.gif"
+  },
+  {
+    "revision": "f9310066628419cc674f8be34baf21a3",
+    "url": "/QueerApp/static/media/wedding.f9310066.jpg"
+  },
+  {
+    "revision": "4e2266543219b9b5cf697f5532a7386b",
+    "url": "/QueerApp/static/media/wrong.4e226654.gif"
+  }
+]);
